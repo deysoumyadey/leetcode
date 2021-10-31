3135ae3b2a44da6a77f9630431f07b81860135a9
deysoumyadey <dey.soumyadey@gmail.com>
2021-10-31 19:48:03 -0400

    initial commit

---
+++ src/main/java/FruitsIntoBaskets.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class FruitsIntoBaskets {
+    public static void main(String[] args) {
+        System.out.println("Max number of fruits picked : " + FruitsIntoBaskets.findMaxFruitsCollected(new int[] {1,2,1}));
+        System.out.println("Max number of fruits picked : " + FruitsIntoBaskets.findMaxFruitsCollected(new int[] {0,1,2,2}));
+        System.out.println("Max number of fruits picked : " + FruitsIntoBaskets.findMaxFruitsCollected(new int[] {1,2,3,2,2}));
+        System.out.println("Max number of fruits picked : " + FruitsIntoBaskets.findMaxFruitsCollected(new int[] {3,3,3,1,2,1,1,2,3,3,4}));
+    }
+    public static int findMaxFruitsCollected(int[] tree){
+        HashMap<Integer, Integer> fruitBasket = new HashMap<Integer, Integer>();
+        int windowStart=0, maxFruits=0;
+        for(int windowEnd=0; windowEnd<tree.length; windowEnd++){
+            int fruit = tree[windowEnd];
+            fruitBasket.put(fruit, fruitBasket.getOrDefault(fruit,0)+1);
+            while(fruitBasket.size()>2){
+                fruitBasket.put(tree[windowStart], fruitBasket.get(tree[windowStart])-1);
+                if(fruitBasket.get(tree[windowStart]) == 0)
+                    fruitBasket.remove(tree[windowStart]);
+                windowStart++;
+            }
+            maxFruits = Math.max(maxFruits, windowEnd-windowStart+1);
+
+        }
+        return  maxFruits;
+    }
+
+}
---
+++ src/main/java/ReverseInteger.java
@@ -0,0 +1,16 @@
+public class ReverseInteger {
+    public static int reverse(int x){
+        int result=0;
+        while(x/10>Math.pow(-2,31)){
+            result=(result*10)+(x%10);
+            x=x/10;
+            System.out.println(x/10);
+            System.out.println(x%10);
+            System.out.println();
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        System.out.println(reverse(112301));
+    }
+}
